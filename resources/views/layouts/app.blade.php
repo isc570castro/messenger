53c18aac45090fe572178f437e416420799b2d7e
ammmecdev <sistema_erp@ammmec.com>
2019-09-02 15:37:34 -0500

    Login UI advances

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
+</head>
+<body>
+    <div id="app">
+        <b-navbar toggleable="sm" type="dark" variant="dark">
+            <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
+
+            <b-navbar-brand href="{{ url('/') }}">
+                {{ config('app.name', 'Laravel') }}
+            </b-navbar-brand>
+
+            <b-collapse id="nav-text-collapse" is-nav>
+           
+                <b-navbar-nav class="ml-auto">
+                    @guest
+                        <b-nav-item href="{{ route('login') }}">Ingresar</b-nav-item>
+                        <b-nav-item href="{{ route('register') }}">Registro</b-nav-item>
+                    @else
+                        <b-nav-item-dropdown text="Username" right>
+                            <b-dropdown-item href="#">Cerrar sesión</b-dropdown-item>
+                        </b-nav-item-dropdown>
+                    @endguest
+                </b-navbar-nav>
+                
+            </b-collapse>
+        </b-navbar>
+
+        <nav class="navbar navbar-default navbar-static-top">
+            <div class="container">
+            
+
+                <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                    <!-- Right Side Of Navbar -->
+                    <ul class="nav navbar-nav navbar-right">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li><a href="{{ route('login') }}">Login</a></li>
+                            <li><a href="{{ route('register') }}">Register</a></li>
+                        @else
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <ul class="dropdown-menu">
+                                    <li>
+                                        <a href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            Logout
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            {{ csrf_field() }}
+                                        </form>
+                                    </li>
+                                </ul>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        @yield('content')
+    </div>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
